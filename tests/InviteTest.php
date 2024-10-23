3ad00a70ebba5533566c2c1d54df3d264c36b941
Студент 14 Кабинет 409 <409-14@coopteh.local>
2024-10-23 09:10:07 +0300

    Добавлен файл

--- tests/InviteTest.php
+++ tests/InviteTest.php
@@ -8,7 +8,7 @@ class InviteTest extends TestCase {
     public function test_router() {
         $invite = new Router();
         $html = $invite->route( "http://localhost/" );
-        $pos= mb_strpos($html, "Привет");
+        $pos= mb_strpos($html, "Привет!");
         $this->assertNotEquals(false, $pos);
     }
 }
